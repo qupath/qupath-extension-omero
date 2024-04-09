fb114699387ddded64551672e136c52f1b3c9d7d
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2024-04-09 09:19:02 +0100

    Typo

--- src/main/java/qupath/ext/omero/core/ObjectPool.java
+++ src/main/java/qupath/ext/omero/core/ObjectPool.java
@@ -40,8 +40,8 @@ public class ObjectPool<E> implements AutoCloseable {
     /**
      * Create the pool of objects. This will not create any object yet.
      *
-     * @param size  the capacity of the pool
-     * @param objectCreator  a function to create an object
+     * @param size  the capacity of the pool (greater than 0)
+     * @param objectCreator  a function to create an object. It is allowed to return null
      * @throws IllegalArgumentException when size is less than 1
      */
     public ObjectPool(int size, Supplier<E> objectCreator) {
