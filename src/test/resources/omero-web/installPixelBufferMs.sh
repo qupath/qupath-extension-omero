95585dc85ea324e84ec5e5fbfdbd4f5520f00d3f
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2024-09-19 16:14:00 +0100

    Java version on test script fix

--- src/test/resources/omero-web/installPixelBufferMs.sh
+++ src/test/resources/omero-web/installPixelBufferMs.sh
@@ -11,10 +11,10 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 # Build the pixel buffer microservice
-yum install git java-devel -y
+yum install git java-11-openjdk-devel -y
 git clone https://github.com/glencoesoftware/omero-ms-pixel-buffer
 cd /omero-ms-pixel-buffer
-export JAVA_HOME=/usr/lib/jvm/jre
+export JAVA_HOME=/usr/lib/jvm/jre-11
 ./gradlew installDist
 
 # The rest is in a different script because the gradlew command was always exiting
