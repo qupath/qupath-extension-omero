30cc4ab4cb14d9250c9c012e2f6bc24ff05b7638
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2024-11-05 17:01:27 +0000

    Unused import

--- src/main/java/qupath/ext/omero/core/WebClient.java
+++ src/main/java/qupath/ext/omero/core/WebClient.java
@@ -24,7 +24,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
