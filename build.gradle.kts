bdf12b9dda3817a1c6762d9a7a7726f2cf18849a
lleplat <lleplat@ed.ac.uk>
2026-03-25 13:47:54 +0000

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 qupathExtension {
     name = "qupath-extension-omero"
     group = "io.github.qupath"
-    version = "0.2.3"
+    version = "0.2.4"
     description = "QuPath extension to support image reading using OMERO APIs"
     automaticModule = "io.github.qupath.extension.omero"
 }
